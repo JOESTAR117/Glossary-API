2278ee8b679b1e60f35f72090f90c8490f043b93
JOESTAR117 <juniorrios1231@gmail.com>
2023-07-05 10:33:53 -0300

    feat: DbContext created

---
+++ Glossary.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Glossary.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Glossary.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Book> books { get; set; }
+    }
+}
